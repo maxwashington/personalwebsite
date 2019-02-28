e6f50ea3dadb2e2aa54a5f18bfa6c1200ad91a63
maxwashington <mnwashington18@gmail.com>
2019-02-28 15:36:47 -0600

    themes black and grey created index file for skills created index file for footer created index file for portfolio created

--- website/src/App.js
+++ website/src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from 'react';
+import styled from 'styled-components';
+import AboutMe from './components/AboutMe/index';
+import black from './assets/themes/black'
+
+const StyledDiv = styled.div``;
+
+const MiddlePart = styled.div`
+  background: ${black};
+  height: auto;
+
+  @media (min-width: 1000px) {
+    padding-bottom: 10px;
   }
-}
 
-export default App;
+  @media (max-width: 1000px) {
+    margin: 0;
+  }
+`;
+
+export default () => (
+  <StyledDiv>
+    <AboutMe/>
+  </StyledDiv>
+);
